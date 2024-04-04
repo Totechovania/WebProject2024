4ca5783ae105eafc59760801efd28373aae0f5b2
Totechovania <bi068612@gmail.com>
2024-04-04 20:40:31 +0300

    funtion generation updated

--- GraphDrawer/function_generation.py
+++ GraphDrawer/function_generation.py
@@ -1,8 +1,6 @@
 from math import cos, sin, tan, gamma
 
 
-
-
 def fac(x):
     return gamma(x)
 
@@ -11,24 +9,35 @@ def tg(x):
     return tan(x)
 
 
-
-def generate_function(formulas, variables):
-    var_init = ''.join((' ' * 4 + f'{k} = {v}\n' for k, v in variables.items()))
-    f_return = ', '.join(formulas)
-    # todo Добавить обаботку исключений
-    code = f'def fun(x, y, time=0):\n{var_init}    return {f_return}'
-
-    loc = {'cos': cos, "sin": sin, 'tan': tan, 'gamma': gamma, 'fac': fac, 'tg': tg}
-
-    exec(code, loc)
-    return loc['fun']
-
-
 def shift_code(text, level):
     res = ''
     for line in text.split('\n'):
         res += ' ' * 4 * level + line + '\n'
     return res
+
+
+def generate_graph_fun(formulas, variables):
+    with open('code_templates/graph_template.txt', 'rt',  encoding='utf') as f:
+        graph_template = f.read()
+
+    code = f"def fun(x, y, {', '.join(variables.keys())}, time=0):\n"
+
+    for i, formula in enumerate(formulas):
+        formula_code = graph_template.format(ind=i, expression=formula)
+        formula_code = shift_code(formula_code, 1)
+        code += formula_code
+
+    f_return = 'return ' + ', '.join(f'f{i}' for i in range(len(formulas)))
+    f_return = shift_code(f_return, 1)
+
+    code += f_return
+
+    loc = {'cos': cos, "sin": sin, 'tan': tan, 'gamma': gamma, 'fac': fac, 'tg': tg}
+
+    print(code)
+    exec(code, loc)
+
+    return loc['fun']
 
 
 def generate_vars_fun(variables):
