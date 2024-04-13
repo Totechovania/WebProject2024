ceb7e1d5eee50956a6212e0e8b07fdc628b5f285
Totechovania <bi068612@gmail.com>
2024-04-13 20:01:01 +0300

    little changes

--- static/js/user_info.js
+++ static/js/user_info.js
@@ -1,13 +1,12 @@
-xmlhttp = new XMLHttpRequest();
+let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let responce = JSON.parse(this.responseText);
-        alert(this.responseText, responce["name"]);
-        document.getElementById('user_img').src = "data:image/png;base64," + responce['img'];
-        document.getElementById('user_name').innerHTML = responce["name"];
-        document.getElementById('user_email').innerHTML = responce["email"];
-        document.getElementById('user_created_date').innerHTML = responce['created_date'];
-        document.getElementById('user_about').innerHTML = responce['about'];
+        if (responce.img) document.getElementById('user_img').src = "data:image/png;base64," + responce.img;
+        if (responce.name) document.getElementById('user_name').innerHTML = responce.name;
+        if (responce.email) document.getElementById('user_email').innerHTML = responce.email;
+        if (responce.created_date) document.getElementById('user_created_date').innerHTML = responce.created_date;
+        if (responce.about) document.getElementById('user_about').innerHTML = responce.about;
     }
 }
 xmlhttp.open("GET", "/api/user_info", true);
